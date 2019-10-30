8978327195c6f7a955d610eecc7f29c54fcbcbf9
Mitchel Meskes <2115621@talnet.nl>
2019-10-30 09:03:42 +0100

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,12 @@
+<?php
+$mensen = array(
+    "Sjonnie de Wiel",
+    "Herman Kaal",
+    "Henk de Steen",
+    "Inge Kerkhoven",
+    "Gert Kruiswijk"
+);
+foreach ($mensen as $array) {
+    echo "$array\n";
+}
+?>
